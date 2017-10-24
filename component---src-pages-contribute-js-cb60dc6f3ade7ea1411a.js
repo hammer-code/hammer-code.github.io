521b9fb080856dc14671830aab8460167f1ff4cb
eezhal92 <arkinthesky.69@gmail.com>
2017-10-24 09:17:19 +0800

    Updates

---
+++ component---src-pages-contribute-js-cb60dc6f3ade7ea1411a.js
@@ -0,0 +1,2 @@
+webpackJsonp([69062039818352],{200:function(e,t,a){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var l=a(2),r=n(l),s=a(23),u=function(e){var t=e.t;return r.default.createElement("div",{className:"container"},r.default.createElement("h1",{className:"crisp crisp--400"},t("contribute")),r.default.createElement("p",null,"We will update this page soon"))};t.default=(0,s.translate)("translations")(u),e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-contribute-js-cb60dc6f3ade7ea1411a.js.map
